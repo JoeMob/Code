e502d5e6e47d559d3a07d8199255fe7e4d4cd55f
JoeMob <WangYuXiaoJoeMob@outlook.com>
2020-04-11 23:58:11 +0800

    JoeMob

--- C/Test/test.c
+++ C/Test/test.c
@@ -1,85 +1,37 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+int tPow(int n)
+{
+    if (n == 0)
+        return 1;
+    else
+        return 10 * tPow(n - 1);
+}
+
 int replaceNumber(int num, int set[])
 {
-    int k = 0;
+    int res = 0;
+    int dig = 0;
+    int temp = 0;
+    int minus = 0;
     if (num < 0)
     {
-        k = 1;
-    }
-    int t = 0;
-    int s = 0;
-    int i = 1;
-    char string[1000] = {'\0'};
-    // if (num < 0)
-    // {
-    //     string[0] = '-';
-    // }
-    while (abs(num) > 0)
-    {
-        string[t] = '0' + abs(num) % 10;
-        t++;
-        num = num / 10;
-    }
-    i = 0;
-    char newString[1000] = {'\0'};
-    while (string[i] != '\0')
-    {
-        if (string[i] == '1')
-        {
-            newString[i] = set[0] + '0';
-        }
-        if (string[i] == '2')
-        {
-            newString[i] = set[1] + '0';
-        }
-        if (string[i] == '3')
-        {
-            newString[i] = set[2] + '0';
-        }
-        if (string[i] == '4')
-        {
-            newString[i] = set[3] + '0';
-        }
-        if (string[i] == '5')
-        {
-            newString[i] = set[4] + '0';
-        }
-        if (string[i] == '6')
-        {
-            newString[i] = set[5] + '0';
-        }
-        if (string[i] == '7')
-        {
-            newString[i] = set[6] + '0';
-        }
-        if (string[i] == '8')
-        {
-            newString[i] = set[7] + '0';
-        }
-        if (string[i] == '9')
-        {
-            newString[i] = set[8] + '0';
-        }
-        i++;
-    }
-    i = 1;
-    while (s < t)
-    {
-        num += (newString[s] - '0') * i;
-        s++;
-        i = i * 10;
-    }
-    if (k = 1)
-    {
         num = -num;
+        minus = 1;
     }
-    // if (string[0] == '-')
-    // {
-    //     num = -num;
-    // }
-    return num;
+    while (num > 0)
+    {
+        temp = num % 10;
+        res += tPow(dig) * set[temp - 1];
+        num = num / 10;
+        dig++;
+    }
+    if (minus == 1)
+    {
+        res = -res;
+    }
+    return res;
 }
 
 int main()
@@ -87,7 +39,8 @@ int main()
     int num;
     int set[9];
     scanf("%d", &num);
-    scanf("%d%d%d%d%d%d%d%d%d", &set[0], &set[1], &set[2], &set[3], &set[4], &set[5], &set[6], &set[7], &set[8]);
+    for (int i = 0; i < 9; i++)
+        scanf("%d", &set[i]);
     printf("%d", replaceNumber(num, set));
     return 0;
 }
