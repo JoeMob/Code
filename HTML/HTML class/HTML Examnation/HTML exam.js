e3fef79daf6e09b1a03bacc954941b108185adeb
JoeMob <WangYuXiaoJoeMob@outlook.com>
2020-06-03 13:14:09 +0800

    HTML class exam.

---
+++ HTML/HTML class/HTML Examnation/HTML exam.js
@@ -0,0 +1,35 @@
+var xhr = new XMLHttpRequest();
+
+function drawQ() {
+    xhr.open("GET", "http://" + document.getElementById("serverIP").value + "/obtain/?studentId=" + document.getElementById("studentID").value);
+    console.log(xhr);
+    xhr.send();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById("drawQOutput").innerHTML = xhr.responseText;
+        }
+    }
+}
+
+function getQ() {
+    xhr.open("GET", "http://" + document.getElementById("serverIP").value + "/obtain/?studentId=" + document.getElementById("studentID").value + "&ordinal=" + document.getElementById("qNumber").value);
+    console.log(xhr);
+    xhr.send();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById("getQOutput").innerHTML = xhr.responseText;
+        }
+    }
+}
+
+function answerQ() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("PUT", "http://" + document.getElementById("serverIP").value + "/submit/?studentId=" + document.getElementById("studentID").value + "&ordinal=" + document.getElementById("qNumber").value + "&answer=" + document.getElementById("answer").value);
+    console.log(xhr);
+    xhr.send();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById("answerQOutput").innerHTML = xhr.responseText;
+        }
+    }
+}
